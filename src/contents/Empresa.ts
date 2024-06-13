8b589a12a33b52ecb0dbc298609a71efe316db3f
Enzo <enzoc@unitech.com.ar>
2024-06-13 18:51:28 -0300

    initial commit

---
+++ src/contents/Empresa.ts
@@ -0,0 +1,23 @@
+export const empresas= [
+    {
+        nombre: "Gatito",
+        descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
+        img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
+    },
+    {
+        nombre: "Gatito",
+        descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
+        img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
+    },
+    {
+        nombre: "Gatito",
+        descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
+        img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
+    },
+    {
+        nombre: "Gatito",
+        descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
+        img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
+    }
+]
+
