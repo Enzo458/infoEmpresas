a988589b1a83dc29553282af55ad11adf31b79e3
Enzo <enzonicolasconti@gmail>
2024-06-17 23:37:50 -0300

    add icon page

--- src/contents/Empresa.ts
+++ src/contents/Empresa.ts
@@ -1,23 +1,51 @@
 export const empresas= [
     {
-        nombre: "Gatito",
+        nombre: "PREMIN",
         descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
         img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
     },
     {
-        nombre: "Gatito",
+        nombre: "GLOBALS GRAINS",
         descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
         img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
     },
     {
-        nombre: "Gatito",
+        nombre: "MOLINOS CLABECQ",
         descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
         img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
     },
     {
-        nombre: "Gatito",
+        nombre: "MARTINEZ Y STANECK",
+        descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
+        img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
+    },
+    {
+        nombre: "DOS CACIQUES",
+        descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
+        img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
+    },{
+        nombre: "BIOTÉCNICAS ARGENTINAS",
+        descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
+        img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
+    },
+    {
+        nombre: "COOPERATIVA AGROPECUARIA",
+        descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
+        img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
+    },
+    {
+        nombre: "ALIMENTOS TANDIL",
+        descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
+        img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
+    },
+    {
+        nombre: "AGROVETERINARIA SCHANG",
+        descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
+        img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
+    },
+    {
+        nombre: "AGROBOLSAS. ",
         descrip: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit, sed do eiusmod tempor",
         img: "https://4.bp.blogspot.com/-3miGETYWTJk/TVmYYFoSlLI/AAAAAAAAABo/CbbdS51k2Bo/s1600/gatito.jpg"      
     }
 ]
-
